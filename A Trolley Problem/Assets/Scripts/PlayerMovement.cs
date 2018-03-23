2139e36a4fad8ff34105fec8f8ffe580fd288c55
Dane Sherman <danemsherman@gmail.com>
2018-03-23 15:01:41 -0400

    lever switches when clicked

--- A Trolley Problem/Assets/Scripts/PlayerMovement.cs
+++ A Trolley Problem/Assets/Scripts/PlayerMovement.cs
@@ -7,14 +7,20 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private float panSpeed;
 
+    [SerializeField] private GameObject Lever; 
+
+    private Camera camera; 
+
     private void Start()
     {
         Cursor.visible = false;
+        camera = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //cardinal movement
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector3.right * moveSpeed);
@@ -31,7 +37,25 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.Translate(Vector3.forward * -moveSpeed);
         }
-        
+
+        //if there's a lever in the scene rotate it when clicked
+        if(Lever != null)
+        {
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+            if (Physics.Raycast(ray, out hit) && hit.collider.tag == "Lever")
+            {
+                Transform objectHit = hit.transform;
+                
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Debug.Log(objectHit.position);
+                    Lever.transform.Rotate(-Lever.transform.rotation.eulerAngles.x*2, 0, 0);
+                }
+            }
+        }
+              
+
         transform.Rotate(0, Input.GetAxis("Mouse X") * panSpeed, 0);
     }
 }
---
+++ A Trolley Problem/Assets/Scripts/SwitchController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour {
+
+    [SerializeField] Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.Rotate(40, 0, 0);
+        }
+	}
+}
